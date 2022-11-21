8eabe9ac74c370ac55fc727f05897aa2749b1208
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2022-11-20 22:22:02 -0300

    Criação inicial da API de Autenticação

---
+++ src/API/InternetBanking.AuthAPI/InternetBanking.AuthAPI/Data/AuthenticationAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetBanking.AuthAPI.Data
+{
+    public class AuthenticationAppDbContext : IdentityDbContext
+    {
+        public AuthenticationAppDbContext(DbContextOptions<AuthenticationAppDbContext> options) : base(options) { }
+    }
+}
