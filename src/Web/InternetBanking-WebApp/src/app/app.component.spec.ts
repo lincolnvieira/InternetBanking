22224bfcebc8d1b3ec84c1ec6009dd06435caafd
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2023-01-06 15:20:34 -0300

    Ajuste de nomenclatura errada e criação inicial da pasta Shared com o componente da barra de navegação

--- src/Web/InternetBanking-WebApp/src/app/app.component.spec.ts
+++ src/Web/InternetBanking-WebApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'InternetBnaking.WebApp'`, () => {
+  it(`should have as title 'InternetBanking-WebApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('InternetBnaking.WebApp');
+    expect(app.title).toEqual('InternetBanking-WebApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('InternetBnaking.WebApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('InternetBanking-WebApp app is running!');
   });
 });
